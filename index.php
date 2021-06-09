a2af52e0004a0f064e96e9dfa773e2f2708dd286
Stephen Igwue <olibenu@yahoo.com>
2021-06-09 15:10:26 +0100

    fix: autoload and licensing

--- index.php
+++ index.php
@@ -2,38 +2,35 @@
 
 declare(strict_types=1);
 
-namespace eyeti\scarf;
-
 require_once(__DIR__ . '/vendor/autoload.php');
 
-require_once(__DIR__ . '/scarf.php');
+require_once(__DIR__ . '/src/scarf.php');
 
-$s = new scarf();
+$s = new \eyeti\scarf\scarf();
 
 //our scarf will call this function
 $tie_function = function (array $request, array $response)
 {	
 	//you might begin by clearing $response too
-	$response = array();
+	//$response = array();
 
 	//this will change the response code from default
 	//$response['code'] = 401;
 
 	//write whatever it is to the data
-	//here, we have taken the liberty of inserting the original request
-	$response['data'] = array(
-		'key' => 'Value can be anything you wish.',
-		'request' => $request
+	/*$response['data'] = array(
+		'key' => 'Value can be anything you wish.'
 	);
+	$response['end'] = gettimeofday(true);*/
 
-	//$response['end'] = gettimeofday(true);
+	//here, we have taken the liberty of inserting the original request
 
 	$response = $request;
 
 	return $response;
 };
 
-scarf::tie($s,
+\eyeti\scarf\scarf::tie($s,
 	[],
 	$tie_function
 );
